2dd67dd111bb81119816dc55b8ea2166c7a08482
huongtxdo <huong.tx.do@gmail.com>
2023-10-02 11:33:13 +0300

    pc update

--- frontend/src/components/LoginAndRegister.tsx
+++ frontend/src/components/LoginAndRegister.tsx
@@ -5,9 +5,21 @@ import RegisterForm from './RegisterForm'
 import Togglable from './Togglable'
 
 const LoginAndRegister = () => {
+  const titleStyle = {
+    fontFamily: 'Arial, sans-serif',
+    fontSize: '36px',
+    textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)',
+  }
   return (
     <>
       <Container>
+        <Row className="justify-content-center">
+          <Col xs={12} md={6}>
+            <div className="mb-4 mt-4">
+              <h2 style={titleStyle}>Blog App</h2>
+            </div>
+          </Col>
+        </Row>
         <Row className="justify-content-center">
           <Col xs={12} md={6}>
             <Togglable buttonLabel="Login here">
